69ea2df10c755c9e0f9ca841219cdd1801edfa01
sabrinadanielasastre@gmail.com <sabrinadanielasastre@gmail.com>
2022-01-31 16:15:26 -0300

    1st commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require ("express");
+const path = require ("path");
+const app = express ();
+
+
+app.listen (3000, () =>{
+console.log ("estoy escuchando en el puerto 3000 DH")
+});
+
+app.get("/home", (req, res) =>{
+    res.sendFile (path.join (__dirname, "./views/index.html"))
+})
+
+app.get ("/babbage", (req, res) =>{
+    res.sendFile (path.join (__dirname, "./views/babbage.html"))
+})
+
+app.get ("/berners-lee", (req, res) =>{
+    res.sendFile (path.join (__dirname, "./views/berners-lee.html"))
+})
+app.get ("/clarke", (req, res) =>{
+    res.sendFile (path.join (__dirname, "./views/clarke.html"))
+})
+app.get ("/hamilton", (req, res) =>{
+    res.sendFile (path.join (__dirname, "./views/hamilton.html"))
+})
+app.get ("/hopper", (req, res) =>{
+    res.sendFile (path.join (__dirname, "./views/hopper.html"))
+})
+app.get ("/lovelace", (req, res) =>{
+    res.sendFile (path.join (__dirname, "./views/lovelace.html"))
+})
+app.get ("/turing", (req, res) =>{
+    res.sendFile (path.join (__dirname, "./views/turing.html"))
+})
+
+app.use (express.static("public"));
+
